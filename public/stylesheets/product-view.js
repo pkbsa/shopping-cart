e34a6234e8de6f4aae1a07ccaa46e8133005598b
pkbsa <siranut.poko@hotmail.com>
2023-02-01 23:26:45 +0700

    add soldout button

--- public/stylesheets/product-view.js
+++ public/stylesheets/product-view.js
@@ -1,3 +1,10 @@
 let carouselInner = document.querySelector('.carousel-inner');
 let firstItem = carouselInner.firstElementChild;
 firstItem.className += " active"
+
+let notAvailableLinks = document.getElementsByClassName("notAvailable");
+
+for (let i = 0; i < notAvailableLinks.length; i++) {
+    notAvailableLinks[i].href = "#";
+    notAvailableLinks[i].textContent = "SOLD OUT";
+}
